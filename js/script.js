1e09bf691694bee109abba99a5934e494991d5c2
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2022-07-25 10:16:17 +0400

    some commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 let musicPlay = document.querySelector('#musicPlay');
-let musicPause=document.querySelector('#musicPause');
 const music = new Audio('music.mp3');
-
+let musicPause = true
 musicPlay.addEventListener("click",function(){
-    music.play()
+    if(musicPause ){
+       
+        music.play()
+        musicPause  = false
+    }else{
+        music.pause()
+        musicPause  = true
+    }
+   
 })
-musicPause.addEventListener("click",function(){
-   music.pause()
-})
+
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -22,17 +27,23 @@ window.onscroll = () =>{
 
 let body = document.querySelector('body')
 let moon =document.querySelector('#moon')
-let sun = document.querySelector('#sun')
 let hed = document.querySelector('header')
 
-moon.addEventListener("click",function(){
-    body.style.background = 'black'
-    hed.style.background = 'black'
-})
 
-sun.addEventListener("click",function(){
-  body.style.background = '#8DA399'
-  hed.style.background = '#8DA399'
+let moonPlay = true;
+
+moon.addEventListener("click",function(){
+    if(moonPlay){
+        body.style.background = 'black'
+        hed.style.background = 'black'
+       
+        moonPlay = false
+    }else{
+        body.style.background = '#8DA399'
+        hed.style.background = '#8DA399'
+        moonPlay = true
+    }
+   
 })
 
 
